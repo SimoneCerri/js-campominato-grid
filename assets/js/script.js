f1e862bacb34a99c342f666b0f60c1b0ff9f93a8
SimoneCerri <iosonosimone@hotmail.it>
2024-02-28 15:16:12 +0100

    html button

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//CLICK on button to generate a grid.
+//addEventListener.
+
+//give to every cell a progressive number(1 - 100).
+//FOR cycle for numbers.
+//add to cells.
+//CLICK on cell to give a console msg with the number.
+//addEventListener on single cell.
+//console.log
