91f8baa144431aa022b10da8c7ddabda72b811a9
SimoneCerri <iosonosimone@hotmail.it>
2024-02-28 16:07:43 +0100

    work on numbers inside square

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,29 @@
 //CLICK on button to generate a grid.
-//addEventListener.
+
 let pressStart = document.getElementById("start_button");
 let container = document.querySelector(".container");
-let markup = `<div class="square"></div>` ;
-let cellsNumbers = 100 ;
+let markup = `<div class="square"></div>`;
+let cellsNumbers = 100;
+
+//addEventListener.
 pressStart.addEventListener("click", function ()
-{
+{   
     for (let x = 0; x < cellsNumbers; x++)
     {
-        container.insertAdjacentHTML("beforeend" , markup);
-        
+        container.insertAdjacentHTML("beforeend", markup);
+    };
+    //give to every cell a progressive number(1 - 100).
+    //FOR cycle for numbers.
+    //add to cells.
+    let squareS = document.getElementsByClassName("square");
+    console.log(squareS);
+    for (let x = 0; x < squareS.length; x++)
+    {
+        const square = squareS[x];
+        console.log(square);
+        square.innerHTML = "1";
     }
 });
-
-//give to every cell a progressive number(1 - 100).
-
-
-//FOR cycle for numbers.
-
-
-//add to cells.
 
 
 //CLICK on cell to give a console msg with the number.
